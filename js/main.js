80e4a18eba8d877eaa2ff33aed4799eee57fb854
rakhmatovdoniyordev <rakhmatovdoniyordev@gmail.com>
2024-08-23 04:02:07 +0500

    completed

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ const collection = document.querySelector(".navbar__collection")
 function navbarToggle(){
     collection.classList.toggle("show")
 }
+
+document.querySelector('.right-btn').addEventListener('click', function() {
+  document.querySelector('.locations__wrapper').scrollBy({
+    left: 150,
+    behavior: 'smooth'
+  });
+});
+
+document.querySelector('.left-btn').addEventListener('click', function() {
+  document.querySelector('.locations__wrapper').scrollBy({
+    left: -150,
+    behavior: 'smooth'
+  });
+});
